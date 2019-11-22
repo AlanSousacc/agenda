75396ab5a397d7dff8eddec5e626bc269446cc94
i12-sistemas <alansousa.cc@gmail.com>
2019-11-22 08:14:44 -0300

    Commit-fist-day

--- public/js/app.js
+++ public/js/app.js
@@ -49525,8 +49525,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\wamp64\www\Laravel 6\FullCalendar\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\wamp64\www\Laravel 6\FullCalendar\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\wamp64\www\Laravel 6\agenda\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\wamp64\www\Laravel 6\agenda\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
