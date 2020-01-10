4bdbc32246ae5bcb3d0de031fad1a5c094977978
AlanSousacc <alansousa.cc@gmail.com>
2020-01-10 16:46:25 -0300

    Commit-for-master-v3

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,8 @@ class CreateUsersTable extends Migration
       $table->string('email')->unique();
       $table->timestamp('email_verified_at')->nullable();
       $table->string('password');
+      $table->enum('profile', ['Administrador', 'Usuario Comum'])->nullable();
+      $table->boolean('isAdmin')->default(0);
       $table->rememberToken();
       $table->timestamps();
     });
