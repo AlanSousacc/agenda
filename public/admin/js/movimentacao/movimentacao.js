895bb4500e156f4bd2f86fbc90d0134c1d4c5abf
Daniel Braga Takegava <48441652+DanielTakegava@users.noreply.github.com>
2020-01-20 19:03:35 -0300

    Run Dev

---
+++ public/admin/js/movimentacao/movimentacao.js
@@ -0,0 +1,144 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 7);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./resources/js/movimentacao/movimentacao.js":
+/*!***************************************************!*\
+  !*** ./resources/js/movimentacao/movimentacao.js ***!
+  \***************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+$('#visualizar').on('show.bs.modal', function (event) {
+  var button = $(event.relatedTarget);
+  var movid = button.data('movid');
+  var tipo = button.data('tipo');
+  var contato_id = button.data('contato_id');
+  var centrocusto_id = button.data('centrocusto_id');
+  var user_id = button.data('user_id');
+  var condicao_pagamento_id = button.data('condicao_pagamento_id');
+  var observacao = button.data('observacao');
+  var valortotal = button.data('valortotal');
+  var valorrecebido = button.data('valorrecebido');
+  var valorpendente = button.data('valorpendente');
+  var movimented_at = button.data('movimented_at');
+  var status = button.data('status');
+  var modal = $(this);
+  modal.find('.modal-body #contato_id').val(contato_id).prop("disabled", true);
+  modal.find('.modal-body #centrocusto_id').val(centrocusto_id).prop("disabled", true);
+  modal.find('.modal-body #tipo').val(tipo).prop("disabled", true);
+  modal.find('.modal-body #user_id').val(user_id).prop("disabled", true);
+  modal.find('.modal-body #condicao_pagamento_id').val(condicao_pagamento_id).prop("disabled", true);
+  modal.find('.modal-body #observacao').val(observacao).prop("disabled", true);
+  modal.find('.modal-body #valortotal').val(valortotal).prop("disabled", true);
+  modal.find('.modal-body #valorrecebido').val(valorrecebido).prop("disabled", true);
+  modal.find('.modal-body #valorpendente').val(valorpendente).prop("disabled", true);
+  modal.find('.modal-body #movimented_at').val(movimented_at).prop("disabled", true);
+  modal.find('.modal-body #status').val(status).prop("disabled", true);
+  modal.find('.modal-body #movid').val(movid);
+});
+$(document).ready(function () {
+  $('#valorpendente').mask('000.000.000.000.000,00');
+  $('#valortotal').mask('000.000.000.000.000,00');
+  $('#valorrecebido').mask('000.000.000.000.000,00');
+});
+
+/***/ }),
+
+/***/ 7:
+/*!*********************************************************!*\
+  !*** multi ./resources/js/movimentacao/movimentacao.js ***!
+  \*********************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! Z:\Laragon\www\agenda\resources\js\movimentacao\movimentacao.js */"./resources/js/movimentacao/movimentacao.js");
+
+
+/***/ })
+
+/******/ });
