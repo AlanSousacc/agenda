1ae4953fa363d3906176525f7f75587b37567eca
Daniel Braga Takegava <48441652+DanielTakegava@users.noreply.github.com>
2020-01-20 18:55:10 -0300

    20/01/2020_18:54_Daniel_Ajuste da V3

--- public/admin/js/movimentacao/movimentacao.js
+++ public/admin/js/movimentacao/movimentacao.js
@@ -136,7 +136,7 @@ $(document).ready(function () {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\wamp64\www\Laravel 6\agenda\resources\js\movimentacao\movimentacao.js */"./resources/js/movimentacao/movimentacao.js");
+module.exports = __webpack_require__(/*! Z:\Laragon\www\agenda\resources\js\movimentacao\movimentacao.js */"./resources/js/movimentacao/movimentacao.js");
 
 
 /***/ })
